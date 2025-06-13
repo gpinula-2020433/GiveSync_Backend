30ec8bdd781558bc09ca433327fffecd0f742058
kterraza-2023022 <kterraza-2023022@kinal.edu.gt>
2025-06-13 13:29:29 -0600

    Update comment.model.js

--- src/comment/comment.model.js
+++ src/comment/comment.model.js
@@ -4,8 +4,8 @@ const commentSchema = Schema(
     {
         content:{
             type: String,
-            maxLength: [500,`Can't be overcome 500, characters` ],
-            required: [true, 'Content is required'],
+            maxLength: [500,`NO puede superar los 500 caracteres` ],
+            required: [true, 'El contenido es requerido'],
         },
         commentImage:{
             type: String,
