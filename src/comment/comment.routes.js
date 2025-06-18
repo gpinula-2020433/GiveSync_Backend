d0336db255f72a7fa7be96c2f28ff8b0e2ffd6db
gpinula-2020433 <gpinula-2020433@kinal.edu.gt>
2025-06-18 03:33:38 -0600

    Se cambian cosas a isClient

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -9,7 +9,7 @@ import {
   getCommentsByUser
 } from "./comment.controller.js";
 
-import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js";
+import { isAdmin, isClient, validateJwt } from "../../middlewares/validate.jwt.js";
 import { uploadProfilePicture } from "../../middlewares/multer.uploads.js";
 import {addCommentV, getCommentsByPublicationV, getCommentsByUserV, updateCommentV} from "../../middlewares/validators.js"
 import { deleteFileOnError } from '../../middlewares/delete.file.on.errors.js';
@@ -23,17 +23,17 @@ api.get("/:id", getCommentById);
 
 api.post(
   "/",
-  [ validateJwt,isAdmin,uploadProfilePicture.single("commentImage"),deleteFileOnError,addCommentV],
+  [ validateJwt, isClient,uploadProfilePicture.single("commentImage"),deleteFileOnError,addCommentV],
   addComment
 );
 
 api.put(
   "/:id",
-  [ validateJwt,isAdmin,uploadProfilePicture.single("commentImage"), updateCommentV],
+  [ validateJwt,isClient,uploadProfilePicture.single("commentImage"), updateCommentV],
   updateComment
 );
 
-api.delete("/:id", [validateJwt, isAdmin], deleteComment);
+api.delete("/:id", [validateJwt, isClient], deleteComment);
 
 api.get('/publication/:publicationId',[getCommentsByPublicationV], getCommentsByPublication)
 api.get('/user/:userId',[getCommentsByUserV],getCommentsByUser)
